860d9513f267acf0c7d2367d3a5c5ed11ec9fd2f
aradaki <takukanaspy@gmail.com>
2021-12-16 11:50:14 +0000

    コメント機能完成?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'tags/index'
-  resources :youtubers
   devise_for :users
+  get 'tags/index'
+  resources :youtubers do
+    resources :comments, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
