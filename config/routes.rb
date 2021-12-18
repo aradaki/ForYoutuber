8f60a5a3f06e7cd3c24aafb6221f7f0a8c42f958
aradaki <takukanaspy@gmail.com>
2021-12-18 14:22:41 +0000

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+} 
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
   get 'tags/index'
+  get "/about" => "about"
+  
   resources :youtubers do
     resources :comments, only: [:create]
   end
